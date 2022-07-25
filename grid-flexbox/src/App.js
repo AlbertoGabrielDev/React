3ceeaf79b688ba0ce5368a4835625f3dbcd25fd5
brasi16 <albertobiel00@hotmail.com>
2022-07-25 19:02:49 -0300

    pokedex

---
+++ grid-flexbox/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <header className='header'>
+        <a href='/'>Logo</a>
+        <nav>
+          <ul className='menu'>
+            <li><a href='/'> Sobre</a></li>
+            <li><a href='/'> Produtos</a></li>
+            <li><a href='/'> Contato</a></li>
+          </ul>
+        </nav>
+
+     </header>
+    </div>
+  );
+}
+
+export default App;
